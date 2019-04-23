bd9d305d41b72fc5a959691fb985b5a9f260479d
Pierce Darragh <pierce.darragh@gmail.com>
2019-04-23 13:52:44 -0600

    Define ProductionPart and subclasses

---
+++ vgf_parser/parse/production_part.py
@@ -0,0 +1,63 @@
+from vgf_parser.tokenize.vgf_token import *
+
+from dataclasses import dataclass
+
+
+class ProductionPart:
+    pass
+
+
+@dataclass
+class ActualPart(ProductionPart):
+    token: VgfToken
+
+
+@dataclass
+class LiteralPart(ActualPart):
+    token: StringToken
+
+
+@dataclass
+class SpecialPart(ActualPart):
+    token: AllCapitalWordToken
+
+
+@dataclass
+class RuleNamePart(ActualPart):
+    token: BracketedTextToken
+
+
+@dataclass
+class ModifiedPart(ProductionPart):
+    actual: ActualPart
+
+
+class OptionalPart(ModifiedPart):
+    pass
+
+
+class ListPart(ModifiedPart):
+    pass
+
+
+class NonemptyListPart(ModifiedPart):
+    pass
+
+
+@dataclass
+class SeparatedPart(ModifiedPart):
+    separator: BracedTextToken
+
+
+class SeparatedListPart(SeparatedPart):
+    pass
+
+
+class NonemptySeparatedListPart(SeparatedPart):
+    pass
+
+
+@dataclass
+class ParameterPart(ProductionPart):
+    name: LowercaseWordToken
+    part: ModifiedPart
