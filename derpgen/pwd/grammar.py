47cead9143a40b879d62c182b0cff0d79e98799b
Pierce Darragh <pierce.darragh@gmail.com>
2019-04-27 01:17:00 -0600

    Define base data types for PwD implementation

---
+++ derpgen/pwd/grammar.py
@@ -0,0 +1,52 @@
+from .tree import Tree
+
+from dataclasses import dataclass
+from typing import Callable, List, TypeVar
+
+__all__ = ['Grammar', 'Nil', 'Eps', 'Tok', 'Rep', 'Alt', 'Seq', 'Red']
+
+
+V = TypeVar('V')
+
+
+@dataclass
+class Grammar:
+    pass
+
+
+@dataclass
+class Nil(Grammar):
+    pass
+
+
+@dataclass
+class Eps(Grammar):
+    trees: List[Tree[V]]
+
+
+@dataclass
+class Tok(Grammar):
+    value: V
+
+
+@dataclass
+class Rep(Grammar):
+    grammar: Grammar
+
+
+@dataclass
+class Alt(Grammar):
+    this: Grammar
+    that: Grammar
+
+
+@dataclass
+class Seq(Grammar):
+    left: Grammar
+    right: Grammar
+
+
+@dataclass
+class Red(Grammar):
+    grammar: Grammar
+    function: Callable[[Tree[V]], Tree[V]]
