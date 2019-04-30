8e6d3437783b74409ad345edc34ff613ef536fe2
Pierce Darragh <pierce.darragh@gmail.com>
2019-04-30 12:39:17 -0600

    Allow additional arguments in match

--- derpgen/pwd/match.py
+++ derpgen/pwd/match.py
@@ -1,5 +1,5 @@
 from inspect import signature
-from typing import Callable, Dict, Tuple, Type, TypeVar, Union
+from typing import Any, Callable, Dict, Optional, Tuple, Type, TypeVar, Union
 
 
 __all__ = ['match']
@@ -16,15 +16,18 @@ def get_param_names(func: Callable) -> Tuple[str]:
     return tuple(sig.parameters.keys())
 
 
-def match(table: Dict[Type, Union[Val, Callable[..., Val]]]) -> Callable[[Arg], Val]:
+def match(table: Dict[Type, Union[Val, Callable[..., Val]]], params: Optional[Tuple[str, ...]] = None,
+          match_arg_pos: int = -1) -> Callable[[Arg], Val]:
     names = {t: get_param_names(table[t]) for t in table}
 
-    def do_match(x: Arg):
+    def do_match(*args: Any):
+        x = args[match_arg_pos]
         cls = x.__class__
         v = table[cls]
         if callable(v):
-            attrs = (getattr(x, name) for name in names[cls])
-            return v(*attrs)
+            param_args = {param: args[i] for (i, param) in enumerate(params)}
+            call_args = (param_args[name] if name in param_args else getattr(x, name) for name in names[cls])
+            return v(*call_args)
         else:
             return v
 
