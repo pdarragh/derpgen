c1c836f74b48f7616d1fe556118c396f00bed5e7
Pierce Darragh <pierce.darragh@gmail.com>
2019-04-30 14:06:30 -0600

    Add predicated match implementation

--- derpgen/pwd/match.py
+++ derpgen/pwd/match.py
@@ -2,7 +2,7 @@ from inspect import signature
 from typing import Any, Callable, Dict, Optional, Tuple, Type, TypeVar, Union
 
 
-__all__ = ['match']
+__all__ = ['match', 'match_pred']
 
 
 Arg = TypeVar('Arg')
@@ -32,3 +32,26 @@ def match(table: Dict[Type, Union[Val, Callable[..., Val]]], params: Optional[Tu
             return v
 
     return do_match
+
+
+def match_pred(table: Dict[Type, Dict[Callable[..., bool], Union[Val, Callable[..., Val]]]],
+               params: Optional[Tuple[str, ...]] = None, match_arg_pos: int = -1) -> Callable[[Arg], Val]:
+    names = {t: {p: get_param_names(v) for p, v in table[t].items()} for t in table}
+    pred_names = {t: {p: get_param_names(p) for p in table[t]} for t in table}
+
+    def do_pred_match(*args: Any):
+        x = args[match_arg_pos]
+        cls = x.__class__
+        preds = table[cls]
+        for pred, v in preds.items():
+            param_args = {param: args[i] for (i, param) in enumerate(params)}
+            pred_args = (param_args[name] if name in param_args else getattr(x, name) for name in pred_names[cls][pred])
+            if not pred(*pred_args):
+                continue
+            if callable(v):
+                call_args = (param_args[name] if name in param_args else getattr(x, name) for name in names[cls][pred])
+                return v(*call_args)
+            else:
+                return v
+
+    return do_pred_match
