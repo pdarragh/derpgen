18f6d81df0599c417350944818666b0dfe03505e
Pierce Darragh <pierce.darragh@gmail.com>
2019-04-29 18:47:57 -0600

    Fix memoization

--- derpgen/pwd/memoize.py
+++ derpgen/pwd/memoize.py
@@ -6,32 +6,33 @@ from typing import Any, Callable, Dict, Tuple, TypeVar
 __all__ = ['EqType', 'memoize']
 
 
+Args = Tuple[Any, ...]
+Key = Tuple[int, ...]
+Val = TypeVar('Val')
+
+
 class EqType(Enum):
     Eq = 1      # Physical equality (pointer equality).
     Equal = 2   # Value equality.
 
 
-def get_hash(o: Any, eq: EqType) -> int:
-    if eq is EqType.Eq:
+def get_hash(eq_type: EqType, o: Any) -> int:
+    if eq_type is EqType.Eq:
         return id(o)
-    elif eq is EqType.Equal:
+    elif eq_type is EqType.Equal:
         return hash(o)
     else:
-        raise RuntimeError(f"Invalid EqType: {eq}.")
-
-
-Key = Tuple[Any, ...]
-Val = TypeVar('Val')
+        raise RuntimeError(f"Invalid EqType: {eq_type}.")
 
 
 def memoize(*eqs: EqType):
-    cache: Dict[Key, Val] = {}
+    cache: Dict[Args, Val] = {}
 
     @wraps
     def decorate(func: Callable[[Any, ...], Val]):
         def wrapper(*args: Any):  # This decorator does not support keyword arguments.
-            key = tuple(get_hash(arg, eqs[i]) for (i, arg) in enumerate(args))
-            val = cache.get(key)
+            key: Key = tuple(get_hash(eqs[i], arg) for (i, arg) in enumerate(args))
+            val: Val = cache.get(key)
             if val is None:
                 val = func(*args)
                 cache[key] = val
