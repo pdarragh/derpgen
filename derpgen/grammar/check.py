f4a7e99320839c8fdf446dfe359bbbb5d691c06d
Pierce Darragh <pierce.darragh@gmail.com>
2019-07-30 23:24:50 -0600

    Implement custom CheckerExceptions

--- derpgen/grammar/check.py
+++ derpgen/grammar/check.py
@@ -6,6 +6,35 @@ from typing import Set
 __all__ = ['check_grammar']
 
 
+class CheckerException(Exception):
+    pass
+
+
+class UnusedTokenException(CheckerException):
+    def __init__(self, token: str):
+        super().__init__(f"Token declared but never used: {token}")
+
+
+class NoStartSymbolsException(CheckerException):
+    def __init__(self):
+        super().__init__("No start symbols declared.")
+
+
+class UndefinedTokenException(CheckerException):
+    def __init__(self, token: str):
+        super().__init__(f"Token requires definition: {token}")
+
+
+class UndefinedRuleException(CheckerException):
+    def __init__(self, rule: str):
+        super().__init__(f"Rule requires definition: {rule}")
+
+
+class DuplicateProductionException(CheckerException):
+    def __init__(self, production: str):
+        super().__init__(f"Production defined more than once: {production}")
+
+
 def check_grammar(grammar: ParsedGrammar):
     production_names = set()
     assumed_tokens = set()
@@ -15,13 +44,13 @@ def check_grammar(grammar: ParsedGrammar):
     # Check that all declared tokens are used.
     for token in grammar.token_matchers:
         if token not in assumed_tokens:
-            raise RuntimeError  # TODO
+            raise UnusedTokenException(token)
     # Check that there is a start symbol and that all start symbols exist.
     if not grammar.start_symbols:
-        raise RuntimeError  # TODO
+        raise NoStartSymbolsException()
     for start_symbol in grammar.start_symbols:
         if start_symbol not in grammar.rules:
-            raise RuntimeError  # TODO
+            raise UndefinedRuleException(start_symbol)
 
 
 def check_ast(tree: AST, grammar: ParsedGrammar, production_names: Set[str], assumed_tokens: Set[str]):
@@ -38,28 +67,28 @@ def check_ast(tree: AST, grammar: ParsedGrammar, production_names: Set[str], ass
         elif isinstance(_tree, DeclaredToken):
             # Declared tokens must be defined in the tokens section.
             if _tree.token not in grammar.token_matchers:
-                raise RuntimeError  # TODO
+                raise UndefinedTokenException(_tree.token)
             assumed_tokens.add(_tree.token)
         elif isinstance(_tree, PatternMatch):
             _check_ast(_tree.match)
         elif isinstance(_tree, RuleMatch):
             # Rule matches must refer to existing rules.
             if _tree.rule not in grammar.rules:
-                raise RuntimeError  # TODO
+                raise UndefinedRuleException(_tree.rule)
         elif isinstance(_tree, NamedProduction):
             # Ensure each production's name is unique.
             if _tree.name in production_names:
-                raise RuntimeError  # TODO
+                raise DuplicateProductionException(_tree.name)
             production_names.add(_tree.name)
             for part in _tree.parts:
                 _check_ast(part)
         elif isinstance(_tree, AliasProduction):
             # Aliases must correspond to real rules.
             if _tree.alias not in grammar.rules:
-                raise RuntimeError  # TODO
+                raise UndefinedRuleException(_tree.alias)
         elif isinstance(_tree, Rule):
             for production in _tree.productions:
                 _check_ast(production)
         else:
-            raise RuntimeError  # TODO
+            raise RuntimeError(f"Unknown AST class: {_tree.__class__.__name__}")
     _check_ast(tree)
